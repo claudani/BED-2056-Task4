57ffa0a5780af2912f42cc40efd79de1013a3729
claudani <c.laudani@student.vu.nl>
2019-11-18 22:55:15 +0100

    Final

--- Task4.R
+++ Task4.R
@@ -4,6 +4,9 @@ library(lubridate)
 library(ggplot2)
 library(zoo)
 library(tidyr)
+library(purrr)
+if (!require("forecast")) install.packages("forecast")
+
 
 #Read in csv file
 data <- read.csv("storedata.csv")
@@ -18,9 +21,9 @@ q1 <- data %>%
   group_by(Region, as.yearmon(Order_Date)) %>%
   summarise(Total_Sales = sum(Sales)) 
 
-  colnames(q1)[2] <- "Month_Year"
+colnames(q1)[2] <- "Month_Year"
 
-  
+
 #Q2 Make a plot of the monthly total Sales in Region 1 and Region 13 in 2015, 2016 and 2017
 q2 <- data %>%
   mutate(Order_Date = as.Date(data$Order_Date, format = "%Y-%m-%d")) %>%
@@ -32,7 +35,7 @@ q2 <- data %>%
 
 colnames(q2)[2] <- "Month_Year"
 
-#TODO show in Figure 1
+# show in Figure 1
 ggplot(q2, aes(x=Month_Year, y=Total_Sales, color=Region, fill=Region)) + 
   geom_line() + 
   geom_point(alpha=0.4) +
@@ -42,14 +45,14 @@ ggplot(q2, aes(x=Month_Year, y=Total_Sales, color=Region, fill=Region)) +
 
 #Q3 In Figure 1, identify the months where the total Sales in Region 13 is greater than the total Sales in Region 1
 
-#TODO show in Table 2
+# show in Table 2
 q3 <- q2 %>% 
   spread(Region, Total_Sales) %>% 
   filter(`Region 13` > `Region 1`) #use `` to make the text not a string, but a evaluatable object
-  
+
 #Q4 Find average Profit per Customer_Segment and Product_Category in 2017, for all regions except Region 3, 5 & 8 
 
-#TODO show in Table 3
+# show in Table 3
 q4 <- data %>%
   mutate(Order_Date = as.Date(data$Order_Date, format = "%Y-%m-%d")) %>%
   filter(Order_Date >= as.Date('2017-01-01') & Order_Date <= as.Date('2017-12-31')) %>%
@@ -58,41 +61,85 @@ q4 <- data %>%
   summarise(average_profit = mean(Profit)) %>% 
   arrange(desc(average_profit))
 
-#Q5 You are asked to estimate a SARIMA model on the aggregated monthly Order_Quantity in the: 
-# Customer_Segment; Small Business and Product_Category; Office Supplies
-#TODO - aggregated monthly Order_Quantity in the Customer_Segment; Small Business and Product_Category; Office Supplies
-#The SARIMA model contains the following parameters:
-#p - AR order
-#d - difference order
-#q - MA order
-#P - SAR order
-#D - seasonal difference
-#Q - SMA order
-#S - seasonal period (12 months in these data)
-#TODO iterate: => should get 2500 models
-#p = [0,4]
-#q = [0,4] 
-#d = [0,1]
-#P = [0,4]
-#Q = [0,4]
-#D = [0,1]
-#S = 12
-
-#1. Estimate these models on the monthly data from 2014 through 2016 
 #2. Identify the best SARIMA model on a holdout sample from 2017, based on the smallest RMSE (Root Mean Square Error) 
 #3. Produce a plot of the whole time series, and add onto it the 2017 forecast from the best SARIMA model, together with 
 #   the actual aggregated monthly Order_Quantity. This output is Figure 2. 
 #4. Specify what was the best SARIMA model on the plot
 
+#model(25000)
+iterations <- expand.grid(list(
+  p = c(0:4), d = c(0:1), q = c(0:4),
+  P = c(0:4), D = c(0:1), Q = c(0:4),
+  S = 12)) 
+
 q5_data <- data %>%
   mutate(Order_Date = as.Date(data$Order_Date, format = "%Y-%m-%d")) %>%
-  filter(Order_Date >= as.Date('2014-01-01') & Order_Date <= as.Date('2016-12-31')) 
-#TODO add the asmonth() method?
-#TODO model(25000)
-#TODO forecast (for 2017)
-#TODO arrange RMSE (forecast, actual) ⇒ Best model!
+  filter(
+    Order_Date >= as.Date('2014-01-01') & Order_Date <= as.Date('2016-12-31'),
+    Customer_Segment == "Small Business",
+    Product_Category == "Office Supplies"
+  ) %>% 
+  select(Order_Date, Order_Quantity) %>%
+  group_by(Order_Date = as.yearmon(Order_Date)) %>%
+  summarise(Monthly_Order_Quantity = sum(Order_Quantity)) %>% 
+  arrange(Order_Date)
 
+q5_2017 <- data %>%
+  mutate(Order_Date = as.Date(data$Order_Date, format = "%Y-%m-%d")) %>%
+  filter(
+    Order_Date >= as.Date('2017-01-01') & Order_Date <= as.Date('2017-12-31'),
+    Customer_Segment == "Small Business",
+    Product_Category == "Office Supplies"
+  ) %>% 
+  select(Order_Date, Order_Quantity) %>%
+  group_by(Order_Date = as.yearmon(Order_Date)) %>%
+  summarise(Monthly_Order_Quantity = sum(Order_Quantity)) %>% 
+  arrange(Order_Date)
 
+min_rmse <- Inf #set to +infinite to start
+rmse_func = function(pred, label) sqrt(sum((pred - label) ^ 2)) #RMSE formula
 
+#The loop
+for(p in 0:4) for (d in 0:1) for (q in 0:4) for (P in 0:2) for (D in 0:1) for (Q in 0:4) {
+  tryCatch({ #work around errors 
+    arima_model <- 
+      arima(as.numeric(q5_data$Monthly_Order_Quantity), 
+            order = c(p, d, q), 
+            seasonal = list(order = c(P, D, Q), period = 12))   
+    
+    #forecast (for 2017)
+    variable <- forecast(object = arima_model, h = 12)
+    prediction_2017 <- variable$mean
+    
+    #arrange RMSE (forecast, actual) ⇒ Best model
+    rmse <- rmse_func(prediction_2017, q5_2017$Monthly_Order_Quantity)
+    
+    if(rmse < min_rmse) {
+      min_rmse <- rmse
+      best_model <- arima_model
+      print(min_rmse)
+      
+    }
+    
+  }, error = function(e){
+    print(paste(p,d,q,P,D,Q))
+  })
+  
+}
+
+TS <- ts(q5_data$Monthly_Order_Quantity, 
+         start = c(2014,1), end = c(2016,12), frequency = 12) 
+  
+p <- TS %>% 
+  forecast() %>% 
+  autoplot(xlab = "Month-Year", 
+           ylab = "Monthly Order Quantity") +
+  autolayer(ts(q5_2017$Monthly_Order_Quantity, 
+               start=c(2017,1), end=c(2017,12), frequency=12), 
+            series="Actual data") 
+  
+p
+
+best_model
 
 
